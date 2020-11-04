dd0a5736ba1cc19e47fb3030121f0f55ca77c80e
stefandr99 <stefandragoi99@gmail.com>
2020-11-04 14:00:25 +0200

    Added code documentation

--- generator/dependente2.php
+++ generator/dependente2.php
@@ -2,17 +2,25 @@
 
 
 class multimiFunctionale {
-    private $letters;
-    private $rules;
+    private $letters; // literele exercitiului
+    private $rules; // array de reguli
     private $leftRulesArray; // partea stanga a regulilor doar ca in format de array
     private $rightRulesArray; // partea dreapta a regulilor doar ca in format de array
-    private $options;
-    private $leftOptions; // elementele din stanga optiunilor sub forma de array
-    private $solutions;
-    private $answer;
-    private $numberOfLetters;
-    private $numberOfRules;
+    private $options; // optiunile exercitiului. Mereu vor fi 6
+    private $solutions; // array de solutii pentru toate optiunile
+    private $answer; // array-ul ce contine se va returna pentru optiuni. Va fi returnat de forma [[optiune], true/false]
+    private $numberOfLetters; // numarul de litere din exercitiu (5/6)
+    private $numberOfRules; // numarul de reguli (5/6)
 
+    /**
+     * multimiFunctionale constructor.
+     * Se alege random daca vor fi 5 sau 6 litere. La fel si pentru numarul de reguli
+     * Se apeleaza pe rand: createRules() - metoda care va crea regulile exercitiului
+     *                      generateOptions() - se genereaza optiunile din care trebuie alese cele corecte.
+     *                              Aici se apeleaza si functia euristica pentru 2 optiunni care vor fi mereu TRUE.
+     *                      createSolutions() - se calculeaza solutia pentru fiecare optiune
+     *                      correctsProblem() - corecteaza optiunile cu ajutorul array-ului de solutie si pune true/false
+     */
     public function __construct() {
         $this->numberOfLetters = rand(5, 6);
         if($this->numberOfLetters == 5)
@@ -28,15 +36,27 @@ class multimiFunctionale {
         $this->correctsProblem();
     }
 
+    /**
+     * Metoda ce va crea regulile exercitiului
+     * Bucla while scop: se genereaza o regula si nu se iese din bucla pana nu se genereaza una care nu a mai fost
+     *      pana atunci in array-ul de reguli. Scopul e ca partea stanga a regulii sa fie unica.
+     * Partea stanga a regulii: poate fi alcatuita din 1-3 litere. Se face o copie a array-ului de litere din care se scot
+     *      literele care au aparut in stanga regulii. Nu vrem sa apara litere si in stanga si in dreapta unei reguli.
+     *      Array-ul elementLeft - multimea de litere din care este alcatuita partea stanga a regulii. Multime ce va fi
+     *      folosita pentru determinarea solutiilor.
+     * Partea dreapta a regulii: poate fi alcatuita din (1, min(3,count(letterCopy))) deoarece este posibil ca numarul literelor
+     *      ramase in letterCopy sa fie 2 (situatie: 5 litere in total, 3 litere in stanga, raman 2 litere pentru dreapta)
+     *      Se aleg random literele care vor alcatui partea dreapta a regulii.
+     *      Array-ul elementRight - multimea de litere din care este alcatuita partea dreapta a regulii. Aceasta va fi folosita
+     *      pentru determinarea solutiei.
+     */
     private function createRules() {
         for($i = 0; $i < $this->numberOfRules; $i++) {
+            // alege elementul din stanga regulii
             $ok = false;
             $elementLeft = array();
-            // alege elementul din stanga regulii
             while(!$ok) {
-                // numarul de litere din elementul1 al regulii curente
                 $n = rand(1, 3);
-                // copie a vectorului de litere folosita pentru elementul 2 din dependenta din regula. Nu trebuie sa contina elementele din element1
                 $lettersCopy = $this->letters;
                 $rand_keys = array_rand($this->letters, $n);
 
@@ -95,8 +115,16 @@ class multimiFunctionale {
         }
     }
 
-    // Momentan doar optiuni triviale. Adica in dreapta optiunii se afla toate literele din stanga optiunii.
-    // Trebuie sa regandesc cum sa generez optiunile
+    /**
+     * Generarea optiunilor exercitiului
+     * Vor fi generate in pozitii random cu ajutorul euristicii 2 optiuni care sigur vor fi true.
+     * Bucla while functioneaza pe acelasi principiu ca la generarea regulilor. Nu se doreste acelasi element in stanga de doua ori.
+     * Vectorul solutie pentru fiecare optiune va fi mereu initializat cu literele care apar in stanga optiunii. La fel si vectorul
+     *      'option2' care reprezinta multimea raspuns pentru fiecare optiune.
+     * Se scoate din copia lettersCopy literele care au aparut deja in partea stanga a optiunii pentru a nu putea fi realse in dreapta
+     *      optiunii.
+     * Dreapta optiunii: se alege intre 0-2 litere pentru a fi adaugate in multimea de raspuns a fiecarei optiuni
+     */
     private function generateOptions() {
         $r = range(0, 5);
         shuffle($r);
@@ -110,7 +138,6 @@ class multimiFunctionale {
                     while(array_key_exists($heuristicOptions[$heuristicIndex][0], $this->options))
                         $heuristicIndex++;
                 $this->options[$heuristicOptions[$heuristicIndex][0]] = $heuristicOptions[$heuristicIndex][1];
-                $this->leftOptions[$i] = $heuristicOptions[$heuristicIndex][0];
                 $this->solutions[$heuristicOptions[$heuristicIndex][0]] = array();
                 for($l = 0; $l < strlen($heuristicOptions[$heuristicIndex][0]); $l++)
                     array_push($this->solutions[$heuristicOptions[$heuristicIndex][0]], $heuristicOptions[$heuristicIndex][0][$l]);
@@ -191,11 +218,18 @@ class multimiFunctionale {
 
                 $this->sortOptions($option2);
                 $this->options[$option1] = $option2;
-                $this->leftOptions[$i] = $option1;
             }
         }
     }
 
+    /**
+     * Se creeaza solutia pentru fiecare optiune. Initial, multimea de litere e alcatuita din literele elemetului din stanga optiunii.
+     * Freq este un vector de frecventa pentru a se verifica daca s-a utilizat regula R(i).
+     * Contidii bucla while: - daca nu s-a mai efectuat nicio modificare in multimea solutie pe parcursul iteratiei
+     *                       - daca multimea solutiei este deja alcatuita din toate literele posibile
+     * Se parcurge fiecare regula si daca elementul din stanga regulii ⊂ multimea solutie => se adauga in multimea solutie tot ce se afla
+     *      in dreapta regulii. Se marcheaza regula si checkDone = false ca nu se poate iesi inca din calcul
+     */
     private function createSolutions() {
         foreach(array_keys($this->solutions) as $key) {
             $solutionBuilder = $this->solutions[$key];
@@ -218,6 +252,10 @@ class multimiFunctionale {
         }
     }
 
+    /**
+     * Pentru fiecare optiune se verifica daca valoarea ei este egala cu solutia calculata mai sus
+     * Se adauga in array-ul raspuns optiunea si raspunsul pentru aceasta: true/false
+     */
     private function correctsProblem() {
         $result = array();
         foreach(array_keys($this->options) as $key) {
@@ -229,6 +267,9 @@ class multimiFunctionale {
         $this->answer = $result;
     }
 
+    /**
+     * Se returneaza JSON cu intreg exercitul
+     */
     public function generate() {
         $result = array(
             "rules" => $this->rules,
@@ -237,7 +278,10 @@ class multimiFunctionale {
         return json_encode($result);
     }
 
-    // se creeaza toate stringurile de 1, 2 litere (EX: A, B, C, AB, AC, BC)
+    /**
+     * Se creeaza toate stringurile de 1, 2 litere (EX: A, B, C, AB, AC, BC)
+     * @param $result - array-ul in care se vor pune stringurile
+     */
     function createCombinations(&$result) {
         for($i = 0; $i < $this->numberOfLetters; $i++) {
             array_push($result, ($this->letters[$i]));
@@ -245,9 +289,14 @@ class multimiFunctionale {
                 array_push($result, ($this->letters[$i] . $this->letters[$j]));
             }
         }
-        return $result;
     }
 
+    /**
+     * Are acelasi comportament ca metoda de calculare a solutiilor doar ca se contorizeaza numarul de intrari in
+     *      structura if (cate reguli se folosesc)
+     * @param $sol - array de multimi alcatuite din literele combinatiilor de mai sus. Ex: [['A'], ['A', 'B'], ['B', 'C'], etc.]
+     * @return int - numarul de repetari realizate (sau numarul de reguli folosite)
+     */
     private function heuristic(&$sol) {
         $solutionBuilder = $sol;
         $freq = array_fill(0, $this->numberOfRules, 0);
@@ -275,6 +324,7 @@ class multimiFunctionale {
      *      si pentru fiecare combinatie se calculeaza cati pasi se fac pentru a se determina solutia ei.
      * Se sorteaza vectorul descrescator dupa numarul de repetitii
      * Se aleg primele 2 optiuni cu cel mai mare numar de repetitii pentru descoperirea solutiei
+     * Se vor alege 2 optiuni altfel: cea mai complexa optiune alcatuita din 2 litere si cea mai complexa alcatuita din 1 litera
      * @return array de forma array[optiune] = [solutie, repetitii]
      */
     public function calculate() {
@@ -298,7 +348,7 @@ class multimiFunctionale {
         $result = array();
         $index = 0;
         $withTwoLetters = false;
-        // prima optiune euristica va fi cu 2 litere, a doua neaparat cu 1 litera
+        // prima optiune euristica va fi cu 2 litere, a doua neaparat cu 1 litera (sau invers)
         foreach(array_keys($resultCopy) as $key) {
             if(strlen($key) == 2 && $withTwoLetters == false) {
                 sort($resultCopy[$key][0]);
@@ -333,6 +383,10 @@ class multimiFunctionale {
         $solution = array_unique(array_merge($solution, $toAdd));
     }
 
+    /**
+     * Sortarea multimii din dreapta optiunii
+     * @param $options - multimea de litere care alcatuieste dreapta optiunii
+     */
     private function sortOptions(&$options) {
         sort($options);
     }
