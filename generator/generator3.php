28ff5a60de323bdf1d5b116bc99fadc66d188695
stefandr99 <stefandragoi99@gmail.com>
2020-11-11 14:02:33 +0200

    generator3 done

--- generator/generator3.php
+++ generator/generator3.php
@@ -1,14 +1,20 @@
 <?php
 
 class Generator3 {
-    private $enunt;
+    private $exercise;
+    private $options;
+    private $counter;
     private $letters;
     private $permutation;
 
     public function __construct() {
         $json = file_get_contents('php://input');
         $input = json_decode($json, TRUE);
-        $this->enunt = $input["enunt"];
+        $this->exercise = $input["exercise"];
+        $this->counter = $input["options"]["counter"];
+        $this->options = array();
+        for($i = 1; $i <= $this->counter; $i++)
+            array_push($this->options, $input["options"][$i]);
 
         $this->setLettersAndPermute();
         $this->applyPermutation();
@@ -17,7 +23,7 @@ class Generator3 {
 
     private function setLettersAndPermute() {
         $m = array();
-        preg_match('/R\([*#$~_[\]A-Z,]*\)/', $this->enunt, $m);
+        preg_match('/R\([*#$~_[\]A-Z,]*\)/', $this->exercise, $m);
         $expr = substr($m[0], 1);
         $this->letters = array();
         for($l = 0; $l < strlen($expr); $l++) {
@@ -29,30 +35,54 @@ class Generator3 {
     }
 
     private function applyPermutation() {
-        for($i = 0; $i < strlen($this->enunt); $i++) {
-            if($this->enunt[$i] == '*' && $this->enunt[$i + 1] == '_') {
-                $letter = $this->enunt[$i + 3];
-                $this->enunt = substr_replace($this->enunt, $this->permutation[ord($letter) - 65], $i, 5);
+        for($i = 0; $i < strlen($this->exercise); $i++) {
+            if($this->exercise[$i] == '*' && $this->exercise[$i + 1] == '_') {
+                $letter = $this->exercise[$i + 3];
+                $this->exercise = substr_replace($this->exercise, $this->permutation[ord($letter) - 65], $i, 5);
             }
         }
-        print_r($this->enunt);
+        for($op = 0; $op < count($this->options); $op++) {
+            for($i = 0; $i < strlen($this->options[$op]); $i++) {
+                if($this->options[$op][$i] == '*' && $this->options[$op][$i + 1] == '_') {
+                    $letter = $this->options[$op][$i + 3];
+                    $this->options[$op] = substr_replace($this->options[$op], $this->permutation[ord($letter) - 65], $i, 5);
+                }
+            }
+        }
     }
 
     private function applySort() {
-        for($i = 0; $i < strlen($this->enunt); $i++) {
-            if($this->enunt[$i] == '*' && $this->enunt[$i + 1] == '~') {
-                $argumentArr = $this->extractArgumentTilda($i);
-                $this->enunt = substr_replace($this->enunt, $argumentArr, $i, $this->getEnd($i + 2) - $i + 1);
+        for($i = 0; $i < strlen($this->exercise); $i++) {
+            if($this->exercise[$i] == '*' && $this->exercise[$i + 1] == '~') {
+                $argumentArr = $this->extractArgumentTilda($this->exercise, $i);
+                $this->exercise = substr_replace($this->exercise, $argumentArr, $i, $this->getEnd($this->exercise, $i + 2) - $i + 1);
+            }
+             else if($this->exercise[$i] == '*' && $this->exercise[$i + 1] == '#') {
+                $argumentArr = $this->extractArgumentNumberSign($this->exercise, $i);
+                $this->exercise = substr_replace($this->exercise, $argumentArr, $i, $this->getEnd($this->exercise, $i + 2) - $i + 1);
             }
         }
-        print_r($this->enunt);
+        for($op = 0; $op < count($this->options); $op++) {
+            for ($i = 0; $i < strlen($this->options[$op]); $i++) {
+                if($this->options[$op][$i] == '*' && $this->options[$op][$i + 1] == '~') {
+                    $argumentArr = $this->extractArgumentTilda($this->options[$op], $i);
+                    $this->options[$op] = substr_replace($this->options[$op], $argumentArr, $i, $this->getEnd($this->options[$op], $i + 2) - $i + 1);
+                }
+                else if($this->options[$op][$i] == '*' && $this->options[$op][$i + 1] == '#') {
+                    $argumentArr = $this->extractArgumentNumberSign($this->options[$op], $i);
+                    $this->options[$op] = substr_replace($this->options[$op], $argumentArr, $i, $this->getEnd($this->options[$op], $i + 2) - $i + 1);
+                }
+            }
+        }
+        shuffle($this->options); // amestec optiunile
     }
 
-    private function extractArgumentTilda($index) {
+    private function extractArgumentTilda($text, $index) {
         $index += 2; // ca sa ajung fix la indexul de dupa prima paranteza patrata
         $start = $index;
-        $end = $this->getEnd($index);
-        $argument = substr($this->enunt, $start, $end - $start + 1);
+        $end = $this->getEnd($text, $index); // returneaza indexul unde se termina argumentul de sortat
+        $argument = substr($text, $start, $end - $start + 1); // extrage argumentul de sortat
+        // creez un array cu toate elementele din argument (de ex: A > B sau A > > BC etc.)
         $dependencies = array();
         $i = 0;
         while ($i < strlen($argument)) {
@@ -65,27 +95,66 @@ class Generator3 {
             }
             $i++;
         }
-        shuffle($dependencies);
-        $sBuilder = $dependencies[0];
+        shuffle($dependencies); // amestec dependentele
+        $sBuilder = $dependencies[0]; // creez un string cu toate dependentele gasite ca argument
         for ($i = 1; $i < count($dependencies); $i++) {
             $sBuilder = $sBuilder . ", " . $dependencies[$i];
         }
         return $sBuilder;
     }
 
-    private function getEnd($index) {
+    private function extractArgumentNumberSign($text, $index) {
+        $index += 2; // ca sa ajung fix la indexul de dupa prima paranteza patrata
+        $start = $index;
+        $end = $this->getEnd($text, $index); // returneaza indexul unde se termina argumentul de sortat
+        $argument = substr($text, $start, $end - $start + 1); // extrage argumentul de sortat
+        // creez un array cu toate elementele din argument (de ex: A > B sau A > > BC etc.)
+        $orderedArg = array();
+        $i = 0;
+        while ($i < strlen($argument)) {
+            if($argument[$i] == '*' && $argument[$i + 1] == '$') {
+                $i += 3;
+                $start2 = $i;
+                while($argument[$i] != ']')
+                    $i++;
+                array_push($orderedArg, substr($argument, $start2, $i - $start2));
+            }
+            $i++;
+        }
+        sort($orderedArg); // ordonez literele
+        $sBuilder = $orderedArg[0]; // creez un string cu toate dependentele gasite ca argument
+        for ($i = 1; $i < count($orderedArg); $i++) {
+            $sBuilder = $sBuilder . $orderedArg[$i];
+        }
+        return $sBuilder;
+    }
+
+    private function getEnd($text, $index) {
         $openPharantesis = 1;
         while ($openPharantesis) {
             $index++;
-            if($this->enunt[$index] == '[')
+            if($text[$index] == '[')
                 $openPharantesis++;
-            if($this->enunt[$index] == ']')
+            if($text[$index] == ']')
                 $openPharantesis--;
         }
         return $index;
     }
 
+    public function generate() {
+        $option["counter"] = $this->counter;
+        for ($i = 1; $i <= $this->counter; $i++)
+            $option[$i] = $this->options[$i - 1];
+
+        $result = array(
+            "exercise" => $this->exercise,
+            "options" => $option
+        );
+        return json_encode($result);
+    }
+
 }
 $g = new Generator3();
+print_r($g->generate());
 
 
