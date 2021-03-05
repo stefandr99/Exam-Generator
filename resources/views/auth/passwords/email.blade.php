709983727599d6c4f6ac7a5f174ee1a11edef91c
stefandr99 <stefandragoi99@gmail.com>
2021-03-05 14:31:14 +0200

    fixed reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title')
+    <title>Resetare parolă</title>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
